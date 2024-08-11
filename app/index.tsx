4f7a540e21508e824f0842c783e2e979290862d1
Rajasekar Thangavel <kt.rajasekar@gmail.com>
2024-08-11 22:27:48 +0530

    Code cleanup

--- app/index.tsx
+++ app/index.tsx
@@ -13,26 +13,25 @@ export default function Page() {
     })
     const API_KEY = "";
     useEffect(() => {
-        const successFn = (position: any) => {
-            setLocation(position.coords)
-            getWeatherData()
-        }
-        const errorCallback = (error: any) => {
-            console.log(error)
-        }
-        navigator.geolocation.getCurrentPosition(successFn, errorCallback)
-        async function getWeatherData() {
+        const onGeolocationSuccess = (geolocation: GeolocationPosition) => {
+            const { latitude, longitude } = geolocation.coords;
+            setLocation({ latitude, longitude });
+            fetchWeatherData();
+        };
+        const handleGeolocationError = (error: GeolocationPositionError) => {
+            console.error(`Failed to get geolocation: ${error.message}`);
+        };
+        if(navigator.geolocation)navigator.geolocation.getCurrentPosition(onGeolocationSuccess, handleGeolocationError)
+        async function fetchWeatherData() {
             const URL = `https://api.tomorrow.io/v4/weather/forecast?location=${location.latitude},${location.longitude}&apikey=${API_KEY}`
             try {
                 const response = await fetch(URL);
                 const data = await response.json();
                 console.log(data)
             } catch (error) {
-
             }
         }
        
-
     }, [])
     return <View>
 
